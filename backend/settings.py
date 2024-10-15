0fe41d0dc55f44e41ca5831f64d50db0c651e6c9
CHdev <codigo.turismointeractivo360@gmail.com>
2024-10-15 14:36:49 -0500

    Preparado para Heroku

--- backend/settings.py
+++ backend/settings.py
@@ -109,9 +109,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication', 
+           
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
