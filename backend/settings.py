bcce4bb09029e6060546268ad2bf5ed48a4a2360
CHdev <codigo.turismointeractivo360@gmail.com>
2024-10-15 15:55:32 -0500

    Configurado Amazon S3 para archivos estáticos y de medios

--- backend/settings.py
+++ backend/settings.py
@@ -130,19 +130,18 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configuración de AWS
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = 'tu_access_key_id'
+AWS_SECRET_ACCESS_KEY = 'tu_secret_access_key'
+AWS_STORAGE_BUCKET_NAME = 'tu_nombre_de_bucket'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 AWS_DEFAULT_ACL = 'public-read'
-AWS_LOCATION = 'static'
+AWS_QUERYSTRING_AUTH = False
 
-# Usar S3 solo para archivos de medios, mantener los estáticos locales
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = '/static/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 # Configuración de Django-Heroku
