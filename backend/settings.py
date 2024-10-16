2e67f1f63de7ed8f37e271e7631981f576487d25
CHdev <codigo.turismointeractivo360@gmail.com>
2024-10-15 21:48:17 -0500

    Configurado Amazon S3 para archivos estáticos y de medios

--- backend/settings.py
+++ backend/settings.py
@@ -137,7 +137,6 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
