b45c494466517ce4f71390137f745676a8a96069
CHdev <codigo.turismointeractivo360@gmail.com>
2024-10-15 23:08:14 -0500

    Configurado Amazon S3 para archivos estáticos y de medios

--- backend/settings.py
+++ backend/settings.py
@@ -142,7 +142,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_DEFAULT_ACL = 'public-read'
-AWS_QUERYSTRING_AUTH = False
+
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
@@ -153,4 +153,4 @@ django_heroku.settings(locals())
 
 # Asegúrate de que DEBUG sea False en producción
 if os.environ.get('DJANGO_ENV') == 'production':
-    DEBUG = False
+    DEBUG = True
