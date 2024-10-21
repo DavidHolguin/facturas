1324c4573190eed2993e3eb6fcdef479de89468b
CHdev <codigo.turismointeractivo360@gmail.com>
2024-10-21 14:53:03 -0500

    Horarios para empresas

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,25 +1,65 @@
 from django.contrib import admin
 from .models import Company, Category, Product, BusinessHours, Order, OrderItem, TopBurgerSection, TopBurgerItem, CompanyCategory, Country
-
+from django.utils.html import format_html
 
 class BusinessHoursInline(admin.StackedInline):
     model = BusinessHours
-    can_delete = False
-    verbose_name_plural = 'Horario de atención'
+    extra = 1
+    
+    fieldsets = (
+        ('Lunes', {
+            'fields': ('monday_open', 'monday_close'),
+        }),
+        ('Martes', {
+            'fields': ('tuesday_open', 'tuesday_close'),
+        }),
+        ('Miércoles', {
+            'fields': ('wednesday_open', 'wednesday_close'),
+        }),
+        ('Jueves', {
+            'fields': ('thursday_open', 'thursday_close'),
+        }),
+        ('Viernes', {
+            'fields': ('friday_open', 'friday_close'),
+        }),
+        ('Sábado', {
+            'fields': ('saturday_open', 'saturday_close'),
+        }),
+        ('Domingo', {
+            'fields': ('sunday_open', 'sunday_close'),
+        }),
+    )
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'phone', 'address', 'get_business_hours')
-    search_fields = ('name', 'user__username')
     inlines = [BusinessHoursInline]
+    list_display = ['name', 'get_business_hours']
 
     def get_business_hours(self, obj):
         try:
             hours = obj.business_hours
-            return f"{', '.join(hours.open_days)} {hours.open_time} - {hours.close_time}"
+            schedule = []
+            days_map = {
+                'monday': 'Lunes',
+                'tuesday': 'Martes',
+                'wednesday': 'Miércoles',
+                'thursday': 'Jueves',
+                'friday': 'Viernes',
+                'saturday': 'Sábado',
+                'sunday': 'Domingo'
+            }
+            
+            for day in days_map:
+                open_time = getattr(hours, f'{day}_open')
+                close_time = getattr(hours, f'{day}_close')
+                if open_time and close_time:
+                    schedule.append(f"{days_map[day]}: {open_time.strftime('%H:%M')} - {close_time.strftime('%H:%M')}")
+            
+            return format_html("<br>".join(schedule)) if schedule else "Sin horarios definidos"
         except BusinessHours.DoesNotExist:
-            return "No establecido"
-    get_business_hours.short_description = 'Horario de atención'
+            return "Sin horarios definidos"
+    
+    get_business_hours.short_description = "Horarios de Atención"
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
