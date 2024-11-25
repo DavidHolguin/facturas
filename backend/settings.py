8ce968faf86436149f2428007978bd16e07bba20
CHdev <codigo.turismointeractivo360@gmail.com>
2024-11-25 09:38:08 -0500

    Login

--- backend/settings.py
+++ backend/settings.py
@@ -133,6 +133,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 # Email settings
